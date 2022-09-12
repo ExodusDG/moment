56a7d22d67597dd7a658c5f706d31de58fe561cb
Mykola Pererbenesiuk <sanistray@gmail.com>
2022-09-12 16:57:25 +0300

    speed

--- js/main.js
+++ js/main.js
@@ -111,3 +111,20 @@ function sliderScroll(number) {
 
     sliderWrapper.attr('style', 'transform: translateX(-' + translateWidth + 'px)')
 }
+
+const swiperFeatures = new Swiper('.swiperFeatures', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    preloadImages: false,
+    lazy: true,
+    autoplay: true,
+    loop: true,
+    navigation: {
+        nextEl: '.sliderFeaturesRight',
+        prevEl: '.sliderFeaturesLeft',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
